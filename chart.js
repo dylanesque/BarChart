7e64ce8c223bb3e2400776ffef3d0176abf107b6
Michael Caveney <dylanesque27@gmail.com>
2019-06-23 11:59:34 -0400

    Improved cleanliness of data fetching, created canvas, began creating scales

--- chart.js
+++ chart.js
@@ -1,12 +1,16 @@
 async function barChart() {
-
   // 1. Access data
-  const dataset = await d3.json('https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/GDP-data.json');
+  const initialData = await d3.json(
+    "https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/GDP-data.json"
+  );
 
-  const yAccessor = d => d.data[1];
+  const dataset = initialData.data;
+  console.log(dataset);
+
+  const yAccessor = d => d[1];
 
   const parseDate = d3.timeParse("%Y-%m-%d");
-  const xAccessor = d => parseDate(d.data[0]);
+  const xAccessor = d => parseDate(d[0]);
 
   // 2. Draw chart
   const width = 750;
@@ -17,19 +21,44 @@ async function barChart() {
       top: 40,
       right: 10,
       bottom: 40,
-      left: 50,
-    },
-  }
-  dimensions.boundedWidth = dimensions.width
-    - dimensions.margin.left
-    - dimensions.margin.right
-  dimensions.boundedHeight = dimensions.height
-    - dimensions.margin.top
-    - dimensions.margin.bottom
+      left: 50
+    }
+  };
+  dimensions.boundedWidth =
+    dimensions.width - dimensions.margin.left - dimensions.margin.right;
+  dimensions.boundedHeight =
+    dimensions.height - dimensions.margin.top - dimensions.margin.bottom;
 
-    // 3. Create canvas
+  // 3. Create canvas
+
+  const wrapper = d3
+    .select("chart")
+    .append("svg")
+    .attr("width", dimensions.width)
+    .attr("height", dimensions.height);
+
+  const bounds = wrapper
+    .append("g")
+    .style(
+      "transform",
+      `translate(${dimensions.margin.left}px, ${dimensions.margin.top}px)`
+    );
+
+  // 4. Create scales
+
+  const xScale = d3.scaleLinear()
+    .domain(d3.extent(dataset, xAccessor))
+    .range([0, dimensions.boundedWidth])
+    .nice();
+
+    const binsGenerator = d3.histogram()
+    .domain(xScale.domain())
+    .value(xAccessor)
+    .thresholds(12);
+
+    const bins = binsGenerator(dataset);
+
+
 }
 
-barChart()
-
-
+barChart();
